2e8f7697c0806e72be385a4189218360a5e177f2
shreyad4@iilinois.edu <90356482+shreyad216@users.noreply.github.com>
2021-12-12 18:10:28 -0600

    completed challenge

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 document.addEventListener("DOMContentLoaded", allWorkAndNoPlayisNoFunAtAll, false);
+document.getElementById("content").className= "visible";
+document.getElementById("h1").style.textAlign= "center";
+document.getElementById("content").style.textAlign= "center";
+document.getElementById('hello_world').addEventListener("click", playSound);
 
-var myMessage = ['H', 'e', 'l', 'l', 'o', 'W', 'o', 'r', 'l', 'd'];
+var myMessage = ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'];
 var i = 0, j = 0, mySound;
 
 function allWorkAndNoPlayisNoFunAtAll() {
 	console.log("Get to Play!");
 
 	mySound = document.getElementById("bling");
-
 	while ((i < myMessage.length) && (j < 300)) {
 		document.getElementById('hello_world').innerHTML += myMessage[i];
-		j++;
+		i++;
 	}
+
+}
+
+function playSound() {
+	mySound.play();
+	document.getElementById("hello_world").style.color = "cyan";
+	document.getElementById("hello_world").style.display = "inline";
+	document.getElementById("hello_world").style.fontSize = "30pt";
+	document.getElementById("hello_world").style.fontWeight = "bold";
+	document.getElementById("hello_world").style.outlineStyle = "solid";
+	document.getElementById("hello_world").style.outlineColor = "white";
+	document.getElementById("h1").style.color = "slateBlue";
+
+
+
+
 }
